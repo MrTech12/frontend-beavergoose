f4a8327ce9b9152fa60e9b43ae0dbd9edfe518d8
Sina Nazar Soroush <13730935+MrTech12@users.noreply.github.com>
2022-05-24 20:45:29 +0200

    Add: basic components for functionality
    * Added components for account system
    * Added components for filesharing, since I was already busy with
    creating.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/account/login/login.component';
+import { RegisterComponent } from './components/account/register/register.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { FileOverviewComponent } from './components/fileshare/file-overview/file-overview.component';
+import { FilesComponent } from './components/fileshare/files/files.component';
+import { SendFileComponent } from './components/fileshare/send-file/send-file.component';
+import { RetrieveFileComponent } from './components/fileshare/retrieve-file/retrieve-file.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomepageComponent,
+    FileOverviewComponent,
+    FilesComponent,
+    SendFileComponent,
+    RetrieveFileComponent
   ],
   imports: [
     BrowserModule,
