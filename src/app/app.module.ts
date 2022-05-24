96b39857cf1b591fd41e742457399d946084b9cc
Sina Nazar Soroush <13730935+MrTech12@users.noreply.github.com>
2022-05-24 22:53:12 +0200

    Add: small info for login page
    * Added materialUI.
    * Added a bit of info on the homepage & login page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,17 @@ import { HomepageComponent } from './components/homepage/homepage.component';
 import { FilesComponent } from './components/fileshare/files/files.component';
 import { SendFileComponent } from './components/fileshare/send-file/send-file.component';
 import { RetrieveFileComponent } from './components/fileshare/retrieve-file/retrieve-file.component';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const appRoutes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'files', component: FilesComponent},
+  {path: 'sendfile', component: SendFileComponent},
+  {path: 'retrievefile', component: RetrieveFileComponent} 
+]
 
 @NgModule({
   declarations: [
@@ -22,7 +34,10 @@ import { RetrieveFileComponent } from './components/fileshare/retrieve-file/retr
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
