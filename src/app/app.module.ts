f4822f9c1d328e3793da958996ca7f95663b2088
Sina Nazar Soroush <13730935+MrTech12@users.noreply.github.com>
2022-05-24 21:45:32 +0200

    Edit: fix remainign of unneeded component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/account/login/login.component';
 import { RegisterComponent } from './components/account/register/register.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
-import { FileOverviewComponent } from './components/fileshare/file-overview/file-overview.component';
 import { FilesComponent } from './components/fileshare/files/files.component';
 import { SendFileComponent } from './components/fileshare/send-file/send-file.component';
 import { RetrieveFileComponent } from './components/fileshare/retrieve-file/retrieve-file.component';
@@ -17,7 +16,6 @@ import { RetrieveFileComponent } from './components/fileshare/retrieve-file/retr
     LoginComponent,
     RegisterComponent,
     HomepageComponent,
-    FileOverviewComponent,
     FilesComponent,
     SendFileComponent,
     RetrieveFileComponent
