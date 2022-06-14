9da43b6045ca58c6677e192639bd89a683762b1e
Sina Nazar Soroush <13730935+MrTech12@users.noreply.github.com>
2022-06-14 23:09:58 +0200

    Fix: tokenService test

--- src/app/services/token.service.spec.ts
+++ src/app/services/token.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 
 import { TokenService } from './token.service';
 
@@ -6,7 +8,10 @@ describe('TokenService', () => {
   let service: TokenService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], 
+      providers: [TokenService]
+    });
     service = TestBed.inject(TokenService);
   });
 
